1f882bda82971b790abf324e52c047c96a54b665
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-10-09 16:01:53 +0200

    Update ProbCentralSucccessorFunction.java

--- Practica1/src/practica1/ProbCentralSucccessorFunction.java
+++ Practica1/src/practica1/ProbCentralSucccessorFunction.java
@@ -15,30 +15,35 @@ import java.util.List;
  * @author jordi.donadeu
  */
 public class ProbCentralSucccessorFunction implements SuccessorFunction{
+    @Override
     public List getSuccessors(Object aState) {
-    ArrayList retVal= new ArrayList();
-    ProbCentralBoard board=(ProbCentralBoard) aState;
-    for(int i=0;i<board.getNumAntenas();i++){
-      // FEM UN CAS PER CADA OPERADOR QUE TINGUEM 
-       ProbCentralBoard newBoardU= new ProbCentralBoard(board.getNumAntenas(),board.getMaxPotencia(),board.getAntenas(),board.getDimPlano(),board.getPlano());
-       if (newBoardU.MoverAntena(i, -1,0)) {
-          String S=new String(ProbCentralBoard.MOVER+" "+i+" Up");
-          retVal.add(new Successor(S,newBoardU));
-       }
-       ProbCentralBoard newBoardPU= new ProbCentralBoard(board.getNumAntenas(),board.getMaxPotencia(),board.getAntenas(),board.getDimPlano(),board.getPlano());
-       if (newBoardPU.aumentaPotencia(i)) {
-          String S=new String(ProbCentralBoard.AUMENTAR+" "+i);
-          retVal.add(new Successor(S,newBoardPU));
-       }
-       
-       ProbCentralBoard newBoardPD= new ProbCentralBoard(board.getNumAntenas(),board.getMaxPotencia(),board.getAntenas(),board.getDimPlano(),board.getPlano());
-       if (newBoardPD.disminuyePotencia(i)) {
-          String S=new String(ProbCentralBoard.DISMINUIR+" "+i);
-          retVal.add(new Successor(S,newBoardPD));
-       }
-       
-    }
-     
-     return retVal;
+        ArrayList retVal= new ArrayList();
+        ProbCentralBoard board=(ProbCentralBoard) aState;
+        for(int i=0;i<board.getnclients(); i++){
+            // FEM UN BOARD PER CADA OPERADOR QUE TINGUEM 
+             ProbCentralBoard newBoardMC= new ProbCentralBoard(board.getCentrals(),board.getnclients(),board.getpropc(),board.getDimPlano(),board.getPlano()); //Board per l'operador canviar central
+             //per un client i concret el posem a totes les centrals lliures disponibles, és a dir, que això generarà tants successors com centrals disponibles
+             for (int j=0;j<board.getnum_centrals_disponibles(); j++){
+                 
+                newBoardMC.moureclient(newBoardMC.getClients().get(i), newBoardMC.getcentrals_disponibles().get(j));
+                if newBoardMC.getCentrals().get(j)
+                String S="Moure client "+i+"a central "+j; 
+                retVal.add(new Successor(S,newBoardMC));
+             }
+             ProbCentralBoard newBoardPU= new ProbCentralBoard(board.getNumAntenas(),board.getMaxPotencia(),board.getAntenas(),board.getDimPlano(),board.getPlano());
+             if (newBoardPU.aumentaPotencia(i)) {
+                String S=new String(ProbCentralBoard.AUMENTAR+" "+i);
+                retVal.add(new Successor(S,newBoardPU));
+             }
+
+             ProbCentralBoard newBoardPD= new ProbCentralBoard(board.getNumAntenas(),board.getMaxPotencia(),board.getAntenas(),board.getDimPlano(),board.getPlano());
+             if (newBoardPD.disminuyePotencia(i)) {
+                String S=new String(ProbCentralBoard.DISMINUIR+" "+i);
+                retVal.add(new Successor(S,newBoardPD));
+             }
+
+        }
+
+    return retVal;
   }
 }
