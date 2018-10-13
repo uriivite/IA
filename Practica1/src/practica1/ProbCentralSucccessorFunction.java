eaecf330f554ccde0c14beb486c3357d5c589528
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-10-13 12:34:30 +0200

    Update ProbCentralSucccessorFunction.java

--- Practica1/src/practica1/ProbCentralSucccessorFunction.java
+++ Practica1/src/practica1/ProbCentralSucccessorFunction.java
@@ -6,6 +6,8 @@ import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +21,12 @@ public class ProbCentralSucccessorFunction implements SuccessorFunction{
         for(int i=0;i<board.getNClients(); i++){
             // FEM UN BOARD PER CADA OPERADOR QUE TINGUEM
             double consum = board.getClients().get(i).getConsumo();
-            ProbCentralBoard newBoardMC= new ProbCentralBoard(board.getCentrals(),board.getNClients(), board.getPropc(), board.getPropg()); //Board per l'operador canviar central
+            ProbCentralBoard newBoardMC = null;
+            try {
+                newBoardMC = new ProbCentralBoard(board.getCent(), board.getNClients(), board.getPropc(), board.getPropg()); //Board per l'operador canviar central
+            } catch (Exception ex) {
+                Logger.getLogger(ProbCentralSucccessorFunction.class.getName()).log(Level.SEVERE, null, ex);
+            }
             Centrales CentralsDisponibles = newBoardMC.getCentralsDisponibles(consum);
             //per un client i concret el posem a totes les centrals lliures disponibles, és a dir, que això generarà tants successors com centrals disponibles
              for (int j=0;j<CentralsDisponibles.size(); j++){
