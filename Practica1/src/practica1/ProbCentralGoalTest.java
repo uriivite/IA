f93c0c2b44aca66b965e87f4ce91fa0ccb85fb02
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-10-16 15:12:35 +0200

    En teoria ja està bé la classe

--- Practica1/src/practica1/ProbCentralGoalTest.java
+++ Practica1/src/practica1/ProbCentralGoalTest.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica1;
 
 import aima.search.framework.GoalTest;
-/**
- *
- * @author jordi.donadeu
- */
+
+
 public class ProbCentralGoalTest implements GoalTest{
     public boolean isGoalState(Object aState) {
     ProbCentralBoard board=(ProbCentralBoard)aState;
