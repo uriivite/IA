e3852eec1294343897ec760bf1b3411b8e162bbe
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-10-16 15:58:18 +0200

    En teoria ja està bé

--- Practica1/src/practica1/ProbCentralHeuristicFunction.java
+++ Practica1/src/practica1/ProbCentralHeuristicFunction.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica1;
 
 import aima.search.framework.HeuristicFunction;
-/**
- *
- * @author jordi.donadeu
- */
+
 public class ProbCentralHeuristicFunction implements HeuristicFunction{
       public boolean equals(Object obj) {
       boolean retValue;
@@ -20,11 +12,10 @@ public class ProbCentralHeuristicFunction implements HeuristicFunction{
   
   public double getHeuristicValue(Object state) {
    ProbCentralBoard board=(ProbCentralBoard)state;
-   int heur = 0;
-   /*int cob=board.calculaCobertura();
-   int pot=board.potenciaEfectiva();
-   if (pot>board.getMaxPotencia()) return(java.lang.Integer.MAX_VALUE);
-   heur=((board.getDimPlano()*board.getDimPlano())-cob); */
+   double heur = 0;
+   double consum_clients = board.getConsumClients(); 
+   double consum_centrals= board.getConsumCentrals(); //serà el sumatori del cost de totes les centrals q tinguin algun client
+   heur=(consum_clients - consum_centrals);
    return(heur); 
   }
 }
